c78b3e898332b6cdd4b6bf9e7aeb8afb41ab660b
Zoheir Karam <stressnode@outlook.com>
2022-07-31 09:02:12 -0700

    -

--- TableToCSharp/MainWindow.xaml.cs
+++ TableToCSharp/MainWindow.xaml.cs
@@ -31,40 +31,60 @@ namespace TableToCSharp
 
 		private async void Button_Connect_Click(object sender, RoutedEventArgs e)
 		{
-			this.btnConnect.IsEnabled = false;
-			this.context = new SqlContext(this.txtConnection.Text);
-			this.cboTables.ItemsSource = await this.context.GetTables();
-			this.btnConnect.IsEnabled = true;
+            try
+            {
+				this.btnConnect.IsEnabled = false;
+				this.context = new SqlContext(this.txtConnection.Text);
+				this.cboTables.ItemsSource = await this.context.GetTables();
+				this.btnConnect.IsEnabled = true;
+			}
+			catch (Exception ex)
+            {
+				MessageBox.Show(ex.InnerException?.Message ?? ex.Message, "Exception");
+				this.btnConnect.IsEnabled = true;
+            }
 		}
 
 		private async void Button_GenerateClass_Click(object sender, RoutedEventArgs e)
 		{
-			this.btnGenerateClass.IsEnabled = false;
+            try
+            {
+				if (this.cboTables.SelectedIndex > -1)
+				{
+					this.btnGenerateClass.IsEnabled = false;
 
-			var classOptions = new ConvertOptions
-			{
-				TableName = this.cboTables.Text,
-				Modifier = (Modifier)Enum.Parse(typeof(Modifier), this.cboModifiers.SelectedValue.ToString()),
-				ClassType = (ClassType)Enum.Parse(typeof(ClassType), this.cboObjectTypes.SelectedValue.ToString()),
-				ShowForeignKey = this.chkShowForeignKey.IsChecked ?? false,
-				ShowForeignProperty = this.chkShowForeignProperty.IsChecked ?? false,
-				ShowMaxLength = this.chkShowMaxLength.IsChecked ?? false,
-				ShowPrimaryKey = this.chkShowPrimaryKey.IsChecked ?? false,
-				ShowTableName = this.chkShowTableName.IsChecked ?? false,
-				EnumerateSimilarForeignKeyProperties = this.chkEnumerateSimilarFKProperties.IsChecked ?? false
-			};
+					var classOptions = new ConvertOptions
+					{
+						TableName = this.cboTables.Text,
+						Modifier = (Modifier)Enum.Parse(typeof(Modifier), this.cboModifiers.SelectedValue.ToString()),
+						ClassType = (ClassType)Enum.Parse(typeof(ClassType), this.cboObjectTypes.SelectedValue.ToString()),
+						ShowForeignKey = this.chkShowForeignKey.IsChecked ?? false,
+						ShowForeignProperty = this.chkShowForeignProperty.IsChecked ?? false,
+						ShowMaxLength = this.chkShowMaxLength.IsChecked ?? false,
+						ShowPrimaryKey = this.chkShowPrimaryKey.IsChecked ?? false,
+						ShowTableName = this.chkShowTableName.IsChecked ?? false,
+						EnumerateSimilarForeignKeyProperties = this.chkEnumerateSimilarFKProperties.IsChecked ?? false
+					};
 
-			context.SetTableName(this.cboTables.Text);
+					context.SetTableName(this.cboTables.Text);
 
-			var converter = new CSharpConverter(classOptions);
-			var tableSchemaResult = await context.GetTableData<TableSchemaResult>();
+					var converter = new CSharpConverter(classOptions);
+					var tableSchemaResult = await context.GetTableData<TableSchemaResult>();
 
-			converter.TableSchama = tableSchemaResult;
+					converter.TableSchama = tableSchemaResult;
 
-			var @class = converter.GetHighlightedClass();
+					var @class = converter.GetHighlightedClass();
 
-			this.htmlDisplay.NavigateToString(@class);
-			this.btnGenerateClass.IsEnabled = true;
+					this.htmlDisplay.NavigateToString(@class);
+					this.btnGenerateClass.IsEnabled = true;
+				}
+
+			}
+			catch (Exception ex)
+            {
+				MessageBox.Show(ex.InnerException?.Message ?? ex.Message, "Exception");
+				this.btnGenerateClass.IsEnabled = true;
+			}
 		}
 	}
 }
